1f42b06d74ea3d189beb9ff1c928d91d3d8e58d9
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-19 10:10:45 -0600

    buidle library and schedule components and put them on the grid

---
+++ Desktop/CTS_files_2/bootstrap.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStor, applyMiddleware } from 'redux';
+
+import Home from './components/home';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import './style/main.scss';
+
+function main() {
+    ReactDOM.render(
+        <Provider store={ createStoreWithMiddleware(redycers)}>
+        <Home />
+        </Provider>
+        , document.querySelector('.app-wrapper'));
+    
+}
+
+document.addEventListener('DOMContentLoaded', main);
