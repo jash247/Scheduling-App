9208d0d945fe5771bfeea66b7bb522ef844cda67
Jared Grant <jared.grant@canyonsdistrict.org>
2018-07-23 10:07:12 -0600

    integrated font awesomeand built a custom icon component

--- Desktop/CTS_files_2/style/library/library.js
+++ Desktop/CTS_files_2/style/library/library.js
@@ -1,10 +1,15 @@
 import react, { component } from 'react';
 
+import LibraryCourse from './libraryCourse';
+
 class Library extends component {
     render() {
         return (
             <div className="library">
                <h1 className="library_title">Course Library</h1>
+            <LibraryCourse/>
+            <LibraryCourse/>
+            <LibraryCourse/>          
             </div>
         )
     }
